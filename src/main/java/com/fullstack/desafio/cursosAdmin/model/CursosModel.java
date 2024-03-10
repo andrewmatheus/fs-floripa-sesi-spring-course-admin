f3fec20789613563a4ee2c81dd26831cbd9454ac
andrewmatheus <andrewmatheus@gmail.com>
2024-03-10 15:21:58 -0300

    feacture: [M1S08] Ex 3 - Cursos

---
+++ src/main/java/com/fullstack/desafio/cursosAdmin/model/CursosModel.java
@@ -0,0 +1,40 @@
+package com.fullstack.desafio.cursosAdmin.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CursosModel {
+    private static Integer proximoId = 1;
+    @Getter private static List<CursosModel> cursos = new ArrayList<>();
+
+    @Setter(AccessLevel.NONE) private Integer id;
+    private String nome;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    private static Integer getProximoId() {
+        return proximoId++;
+    }
+
+    public static CursosModel inserir(CursosModel curso) {
+        curso.id = getProximoId();
+        cursos.add(curso);
+        return curso;
+    }
+
+    public static CursosModel buscarPorId(Integer id) throws Exception {
+        for (CursosModel curso : cursos) {
+            if (curso.getId().equals(id)) {
+                return curso;
+            }
+        }
+        throw new Exception("Curso não encontrado");
+    }
+
+}
